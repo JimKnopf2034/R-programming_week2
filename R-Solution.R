83c82ff35245f725363f5538000b3eadcd383068
Otrek Wilke <otrek.wilke@mailbox.org>
2016-02-07 17:29:48 +0100

    created a skeleton and added the data

---
+++ R-Solution.R
@@ -0,0 +1,34 @@
+# This the solution to the R-Programming task in Week 2, written R
+
+# Part 1
+# --------
+#
+# The task is to read in the data in the csv files and calculate the mean of the
+# quantity sulfate or nitrate. Basics steps are:
+# 1. Read data
+# 2. Remove NAs
+# 3. Compute mean()
+pollutantmean <- function(directory, pollutant, id=1:332){
+
+}
+
+# Part 2
+# --------
+#
+#
+complete <- function(directory, id=1:332){
+
+}
+
+# Part 3
+# --------
+#
+#
+corr <- function(directory, threshold=0){
+
+}
+
+# run the functions to test them
+pollutantmean()
+complete()
+corr()
